744fbe5fcaf7b78bc80871821e6e8c002f0b6e75
Mats Gustafsson <mats@mcnab.se>
2025-02-01 13:01:18 +0100

    Define version in pyproject.toml

--- avanza/__init__.py
+++ avanza/__init__.py
@@ -3,5 +3,10 @@ from .ticker import Ticker
 from .search import Search
 from .news import News
 from .collection import Collection
-from ._version import __version__
 from .base import Config, Base
+try:
+    from importlib.metadata import version as get_version
+except ImportError:
+    from importlib_metadata import version as get_version
+
+__version__ = get_version("Avanza")
